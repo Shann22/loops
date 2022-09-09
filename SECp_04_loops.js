4703b1acea0be21d7a479ad09ee6d0d1f7077b6f
Sophia Han <eunjookim@sophia-macbook.myfiosgateway.com>
2022-09-09 13:42:11 -0400

    First Commit

---
+++ SECp_04_loops.js
@@ -0,0 +1,48 @@
+// alert("Hello from JS!")
+
+// 1.
+// Numbers up to ten
+// Using a for loop console.log all the numbers from 0 to 10
+
+// Using a while loop console.log all the numbers from 0 to 10
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// 2.
+// Even numbers to 20
+// Using a for loop console.log all the even numbers from 0 to 20
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+// 3.
+// Odd numbers to 20
+// Using a while loop console.log all the odd numbers from 0 to 20
+
+let num = 1;
+while (num <= 20) {
+    console.log(num);
+    num += 2;
+}
+
+
+// 4.
+// Back to zero
+// Using a for loop console.log all the numbers from 10 to 0.
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+
+
+
+
